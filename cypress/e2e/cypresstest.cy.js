84ae2d85045cf783de11a7b669678e179c625743
Silvia A. Rodriguez <silviarodriguezalonso70@gmail.com>
2024-08-25 20:36:06 -0600

    Ingresando cambios en la forma de dar click en la page cypress

--- cypress/e2e/cypresstest.cy.js
+++ cypress/e2e/cypresstest.cy.js
@@ -1,5 +1,5 @@
-describe('Login', () => {
-    it('login exitoso', () => {
+describe('click for position', () => {
+    it('click en Canva', () => {
       cy.visit('https://example.cypress.io/commands/actions')
       cy.get("#action-canvas").click("topLeft")
       cy.get("#action-canvas").click("top")
@@ -12,9 +12,11 @@ describe('Login', () => {
       cy.get("#action-canvas").click("center")
       cy.get("#action-canvas").click(80,75)
       cy.get("#action-canvas").click(180,75)
-      cy.get(".action-labels > span:nth-of-type(6)").click()
-      cy.get(".action-labels > .label").click({ multiple: true })
-      cy.get(".opacity-cover").click()
+      
+      cy.get(".action-labels > span:nth-of-type(6)").click()    //este es para dar click en un boton especifico que en este caso es el bootn 6
+      cy.get(".action-labels > .label").click({ multiple: true })  // y este es para dar click a los 6 botones
+
+      cy.get(".opacity-cover").click()  // este es para dar click al bootn que esta opacado o que no se puede acceder
       
     })
 })
